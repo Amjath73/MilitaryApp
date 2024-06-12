3635da9f320078340b15a766fad4cbe8d223061a
Amjath73 <amjathsanker4@gmail.com>
2024-06-12 23:30:33 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddMiltary from './components/AddMiltary';
+
+function App() {
+  return (
+    <div>
+      <AddMiltary/>
+    </div>
+  );
+}
+
+export default App;
